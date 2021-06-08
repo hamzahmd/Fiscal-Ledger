dd7ce465e3afe621607ee998d858440294eaaddf
Ecoham <59919892+Ecoham@users.noreply.github.com>
2021-06-08 13:14:52 +0500

    Added some js

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-
+// Google Map API Integration
 function initMap() {
 
   const loc ={lat: 32.506929, lng: 74.524809 };
@@ -12,3 +12,27 @@ function initMap() {
   const marker = new google.maps.Marker({ position:loc,map:
     map});
 }
+// sticky Menu Background
+window.addEventListener('scroll',function(){
+  if (window.scrollY>150){
+    document.querySelector('#navbar').style.opacity=0.9;  
+  } else {
+    document.querySelector('#navbar').style.opacity=1;  
+  }
+});
+
+// For Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event){
+  if(this.hash !== ''){
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+}); 
